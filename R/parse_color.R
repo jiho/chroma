a32487795a15e0139fc1a9ed264c37cf2308a1a5
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-22 23:53:19 +0100

    Stub for a generic color parser

---
+++ R/parse_color.R
@@ -0,0 +1,27 @@
+#' Parse colors specified in a given model
+#'
+#' @param x a matrix or data.frame whose column specify color channels.
+#' @template model
+#'
+#' @family color specifications
+#'
+#' @return A vector of colors specified as hex codes
+#'
+#' @examples
+#' parse_color(data.frame(h=c(0, 120, 240), s=0.5, v=0.7), "hsv")
+#' parse_color(data.frame(r=c(255, 0, 0),
+#'                        g=c(0, 255, 0),
+#'                        b=c(0, 0, 255)), "rgb")
+#'
+#' @export
+parse_color <- function(x, model) {
+  # create command string
+  cmds <- apply(x, 1, function(xx) {
+    paste0("chroma.", model, "([", paste0(xx, collapse=","), "]).hex()")
+  })
+  
+  # convert colors
+  res <- v8_eval(cmds)
+  
+  return(res)
+}
