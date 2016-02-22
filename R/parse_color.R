632314afe3dc3e3ae1c99b35f7486cd0d5a6b6f1
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-23 00:46:47 +0100

    Make parsing more robust

--- R/parse_color.R
+++ R/parse_color.R
@@ -15,12 +15,34 @@
 #'
 #' @export
 parse_color <- function(x, model) {
-  # create command string
+  # recognise color model
+  model <- match.arg(model, c("rgb", "hsv", "hsl", "hcl", "lch", "lab", "cmyk", "css"))
+
+  # check arguments
+  if (model == "css") {
+    x <- as.character(x)
+    # TODO harder check here
+  } else {
+    
+    if (!(is.matrix(x) | is.data.frame(x))) {
+      stop("x should be a matrix or data.frame")
+    }
+
+    required_columns <- switch(model, cmyk = 4, 3)
+    if (ncol(x) < required_columns) {
+      stop("x should have at least ", required_columns, " columns")
+    }
+    if (ncol(x) > required_columns) {
+      warning("only the first ", required_columns, " columns of x will be used")
+    }
+
+  }
+  
+  
+  # parse colors using chroma.js
   cmds <- apply(x, 1, function(xx) {
     paste0("chroma.", model, "([", paste0(xx, collapse=","), "]).hex()")
   })
-  
-  # convert colors
   res <- v8_eval(cmds)
   
   return(res)
