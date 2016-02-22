a676979eed7eb4435a466d8928ce0f61a755c3fa
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-23 00:50:29 +0100

    Improve document and homogenise it with luminance()

--- R/channel.R
+++ R/channel.R
@@ -2,15 +2,31 @@
 #'
 #' @template colors
 #' @template model
-#' @param channel string defining the channel within the color model
+#' @param channel string defining the channel within the color model.
+#' @param value the channel value to set; a number, the convention of which depend on the channel.
 #'
+#' @return
+#' For \code{channel}, a vector of channel values, the convention of which depend on the channel.
+#' 
+#' For \code{channel<-}, the updated object(s).
+#' 
+#' @seealso \code{\link{luminance}} for relative brightness, which is slightly different from the perceived brightness (i.e. the value of the L channel in Lab or HCL).
+
 #' @examples
 #' channel("pink", "rgb", "r")
-#' col <- rainbow(5)
-#' show_col(col)
-#' channel(col, "hcl", "l") <- 0.3
-#' show_col(col)
 #' channel(colors()[1:5], "hcl", "l")
+#'
+#' # colors along a HSV rainbow have very different lightness
+#' channel(rainbow(6), "hcl", "l")
+#' # but lightness should be constant along a HCL "rainbow", by definition
+#' # here subtle changes are caused by the round-trip conversion to R colors
+#' channel(hcl(h=seq(0, 360, length.out=6), l=0.5), "hcl", "l")
+#'
+#' # force a given lightness
+#' x_orig <- x <- rainbow(6)
+#' show_col(x)
+#' channel(x, "hcl", "l") <- 0.3
+#' show_col(x_orig, x)
 #'
 #' @export
 channel <- function(x, model, channel) {
