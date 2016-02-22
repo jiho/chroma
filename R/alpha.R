807f63561bfb545d861b925c9f4dc9fe02d56d45
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-22 17:50:02 +0100

    Importe the alpha function from scales

---
+++ R/alpha.R
@@ -0,0 +1,5 @@
+#' @importFrom scales alpha
+#' @export
+alpha <- scales::alpha
+
+# TODO make a custom function and document the fact that this has to be applied last because it does not carry over to chroma in hex notation
