3d8d2c604f8e6056c2f2bb1f2b25f719bed72185
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-23 10:59:01 +0100

    Improve documentation of color parsing functions

--- R/rgb.R
+++ R/rgb.R
@@ -3,11 +3,18 @@
 #' Create a vector of colors from red, green, and blue.
 #'
 #' @param red,green,blue color channels, numeric vectors with values in \code{[0, maxColorValue]}.
-#' @param alpha transparency, numeric vector with values in \code{[0, maxColorValue]}; 0 means fully transparent, \code{maxColorValue} means fully opaque.
+#' @param alpha transparency, numeric vector with values in \code{[0, maxColorValue]}; 0 means fully transparent, \code{maxColorValue} means fully opaque. See function \code{link{alpha}} for another way to change the transparency after the fact.
 #' @param names character vector. The names for the resulting vector.
 #' @param maxColorValue number giving the maximum of the color values range; typically 1 or 255.
 #'
 #' @template color_spec
+#'
+#' @details
+#' \code{gl} is a variant which forces \code{maxColorValue} to be 1. It is provided for full compatibility with chroma.js.
+#'
+#' RGB is how colors are displayed on a computer screen. However, this is not how colors are perceived by the human eye/brain. Other color spaces such as HCL and L* a* b* make it easier to create color palettes that are appropriate for human perception.
+#'
+#' @export
 #'
 #' @examples
 #' rgb()
@@ -26,9 +33,8 @@
 #'   rgb(g=ramp),
 #'   rgb(b=ramp)
 #' )
-#'
-#' @export
 rgb <- function(red=0, green=0, blue=0, alpha=NULL, names=NULL, maxColorValue=1) {
+  # TODO consider using rgba as the drop-in replacement and warn about it when using rgb
 
   # handle color channels
   x <- tabularise_arguments(red, green, blue)
--- man-roxygen/color_spec.R
+++ man-roxygen/color_spec.R
@@ -1,9 +1,11 @@
 #' @details
-#' The first argument can also be a data.frame or a matrix. In that case, its first three columns are considered as the three color components and the rest of the function arguments are ignored.
+#' The first argument can also be a data.frame or a matrix. In that case, its columns are considered as the color components, taken in order, and the other color components arguments are ignored.
 #'
-#' When the arguments are used separately and are vectors, values in shorter arguments are recycled to match the length of the longest argument. If the lengths are not compatible, an error is output.
+#' When separate arguments are used for the color components and are vectors, values in shorter arguments are recycled to match the length of the longest argument. If the lengths are not compatible, an error is output.
 #'
 #' @family color specifications
 #'
 #' @return A vector of colors specified as hex codes
+#' 
+#' @seealso See \code{\link{parse_color}} for the general function to parse colors in various specifications, which this function calls internally.
 
