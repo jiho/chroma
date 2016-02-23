adf7ad31ead25295bed3ba754e326c202999efba
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-23 12:04:09 +0100

    Parse HCL colors with parse_colors()

--- R/parse_hcl.R
+++ R/parse_hcl.R
@@ -28,53 +28,13 @@
 #' @export
 # TODO make compatible with grDevices::hcl
 hcl <- function(h=0, c=0.6, l=0.6) {
-  if ( is.matrix(h) | is.data.frame(h) ) {
-    # if first argument is a table, use this only
-    d <- h[,1:3]
-  } else {
-    # else, convert the arguments into a table
-    # NB: this does argument recycling on its own; we just provide a nicer error message
-    tryCatch(d <- data.frame(h, c, l),
-      error=function(e) {
-        stop("The lengths of arguments h, c, and l are not compatible.", call.=F)
-      }
-    )
-  }
-  
-  # check that argument values are correct
-  if (any(d[,1] < 0)) {
-    stop("h cannot be < 0")
-  }
-  if (any(d[,1] > 360)) {
-    stop("h cannot be > 360")
-  }
-  if (any(d[,2] < 0)) {
-    stop("c cannot be < 0")
-  }
-  if (any(d[,2] > 1)) {
-    stop("c cannot be > 1")
-  }
-  if (any(d[,3] < 0)) {
-    stop("l cannot be < 0")
-  }
-  if (any(d[,3] > 1)) {
-    stop("l cannot be > 1")
-  }
-  
-  # chroma.js works with percentages here
-  d[,2:3] <- d[,2:3] * 100
-  
-  # convert each row of input
-  apply(d, 1, function(x) {
-    chroma.hcl(x[1], x[2], x[3])
-  })
-}
+  # handle color channels
+  x <- tabularise_arguments(h, c, l)
 
-
-chroma.hcl <- function(h, c, l) {
-  ct <- v8_chroma_context()
-  cmd <- paste0("chroma.hcl(", h, ",", c, ",", l, ").hex()")
-  ct$eval(cmd)
+  # parse colors using chroma.js
+  colors <- parse_color(x, "hcl")
+    
+  return(colors)
 }
 
 #' @name hcl
