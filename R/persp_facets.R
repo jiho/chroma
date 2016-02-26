6bec0790ee77a1a1388593aa675848e49f08ca17
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-26 17:13:50 +0100

    Add function to compute values of facets for persp() calls

---
+++ R/persp_facets.R
@@ -0,0 +1,23 @@
+#' Facets values in a persp plot
+#'
+#' Compute the value at the center of facets in a perspective plot of a matrix
+#' 
+#' @param z a numeric matrix to be used as the z argument of \code{link[graphics]{persp}}
+#' 
+#' @return A matrix of dimensions \code{[nrow(z)-1, ncol(z)-1]} with the values at the center of each facet
+#'
+#' @examples
+#' z <- matrix(1:12, nrow=4)
+#' persp_facets(z)
+#'
+#' @export
+persp_facets <- function(z) {
+  if (is.matrix(z) | !is.numeric(z)) {
+    stop("z needs to be a numeric matrix")
+  }
+  ncz <- ncol(z)
+  nrz <- nrow(z)
+  # value at facet center
+  zf <- (z[-1, -1] + z[-1, -ncz] + z[-nrz, -1] + z[-nrz, -ncz]) / 4
+  return(zf)
+}
