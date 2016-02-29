55e3ffebf233738b0b3385ddf9c91e83473404f5
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-29 09:08:20 +0100

    Lightness does not go all the way to white

--- man-roxygen/param_lightness.R
+++ man-roxygen/param_lightness.R
@@ -1,1 +1,1 @@
-#' @param l lightness, numeric vector with values in \code{[0, 1]}; 0 is black, 1 is white.
+#' @param l lightness, numeric vector with values in \code{[0, 1]}; 0 is darkest, 1 is brightest.
