f8de160695269e8bc404a4fc76900f6f9013a581
Jean-Olivier Irisson <irisson@normalesup.org>
2016-02-29 10:02:33 +0100

    See also bits do not need "See", it's implied

--- man-roxygen/color_spec.R
+++ man-roxygen/color_spec.R
@@ -1,4 +1,4 @@
 #' @family color specifications
 #'
-#' @seealso See \code{\link{parse_color}} for the general function to parse colors in various specifications (which this function calls internally) and \code{\link{convert_color}} to convert parsed colors to another model.
+#' @seealso \code{\link{parse_color}} for the general function to parse colors in various specifications (which this function calls internally) and \code{\link{convert_color}} to convert parsed colors to another model.
 
