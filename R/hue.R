a4e397ac8f1c2f91f95ae13329e134f602549a8a
Jean-Olivier Irisson <irisson@normalesup.org>
2017-08-08 22:43:38 +0200

    Improve documentation

--- R/hue.R
+++ R/hue.R
@@ -1,33 +1,33 @@
 #' Get the hue of colors
 #'
-#' @param h hue, either angles around the color wheel, in \code{[0, 360]} (angles outside of the range are rotated back to within \code{[0, 360]}: 380 = 20, -15 = 345, etc.), or colors (hex or named) from which the hue is extracted.
+#' Get the hue (angle around the color wheel, in [0,360]) of a vector of colors.
+#'
+#' @template param_x_rcolors
 #' @param model string defining the color model; valid models are the ones containing a hue component: \code{hsv}, \code{hsl}, \code{hsi}, \code{hcl}, \code{lch}.
 #'
-#' @return A vector of hue angles, in \code{[0, 360]}.
+#' @return A vector of hue angles, in [0,360].
 #'
-#' @seealso \code{\link{channel}} used to extract the hue channel.
+#' @seealso \code{\link{channel}} used internally, to extract the hue channel.
+#'
+#' @export
 #'
 #' @examples
 #' # h can be numbers, hex colors, named colors, or a mix of the above
-#' hue(c(0, 10, 200))
 #' hue(c("#F55D5B", "#16b2b4", "#6A9F16"))
 #' hue(c("red", "green", "blue"))
+#' hue(c(0, 10, 365))
 #' hue(c(10, "#B55FFC", "pink", 365))
 #'
 #' # It should be possible to reconstruct colors similar to the original ones
-#  # based on the extracted hue, by providing the other components
-#' cols <- c("firebrick", "gold", "limegreen", "dodgerblue", "navy", "mediumpurple")
+#' # based on the extracted hue, by providing the other components
+#' cols <- c("firebrick", "gold", "limegreen", "dodgerblue",
+#'           "navy", "mediumpurple")
 #' show_col(cols,
-#'          hsv(h=hue(cols, model="hsv"), s=0.9, v=1),
-#'          hsl(h=hue(cols, model="hsl"), s=1, l=0.6),
-#'          hcl(h=hue(cols, model="hcl"), c=1, l=0.8))
-#' # = works, but is a bit less reliably with hcl().
-#'
-#' @export
-hue <- function(h, model="hsv") {
-  if (is.numeric(h) | is.logical(h)) {
-    # NB: all NA vectors are logical and handled here
-    out <- h %% 360
+#'   hsv(h=hue(cols, model="hsv"), s=0.9, v=1),
+#'   hsl(h=hue(cols, model="hsl"), s=1, l=0.6),
+#'   hcl(h=hue(cols, model="hcl"), c=1, l=0.8)
+#' )
+#' # = works, but is a bit less reliable with hcl().
   } else {
     # deal with partially "numeric" vectors
     suppressWarnings(numh <- as.numeric(h))
