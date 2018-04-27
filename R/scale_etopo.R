6c877190480be060124499d42b06354ef0fa409f
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-27 22:42:47 +0200

    Add etopo scale

---
+++ R/scale_etopo.R
@@ -0,0 +1,74 @@
+#' Colors from ETOPO1
+#'
+#' @format A data.frame with 43 rows and 2 variables:
+#' \describe{
+#'   \item{altitude}{the altitude.}
+#'   \item{color}{the hex code of the correpsonding color.}
+#' }
+#' @source \url{http://soliton.vm.bytemark.co.uk/pub/cpt-city/ngdc/tn/ETOPO1.png.index.html}
+#' @seealso \code{\link{etopo_scale}} to use the palette.
+"etopo"
+
+
+#' ETOPO1 scales and palettes
+#'
+#' Color scales and palettes inspired from the representation of the ETOPO1 global relief model. These scales are special in that the mapping between colors and values is predefined to work for altitudes (or depths when the numbers are negative).
+#'
+#' @inheritParams interp_scale
+#'
+#' @template return_scales
+#'
+#' @family color scales and palettes
+#'
+#' @references The topographic colors are based on GMT globe by Lester M. Anderson of CASP, UK, modified by Jesse Varner and Elliot Lim (NOAA/NGDC) to have a smaller band of white at the highest elevations. The bathymetry is based on GMT haxby, popularised by Bill Haxby, LDEO. See \url{https://www.ngdc.noaa.gov/mgg/global/global.html}.
+#' @seealso \code{\link{etopo}} for the colors in the palette and the associated altitudes.
+#'
+#' @examples
+#' # Defining a scale in that case has little value since it cannot be
+#' # customized. Explore the default mapping
+#' altitudes <- c(-5000, -1000, -10, 0, 10, 200, 500, 5000)
+#' show_col(etopo_map(altitudes))
+#' # Explore the full range of colors
+#' show_col(etopo_map(seq(-8000, 8000, length=500)))
+#'
+#' # It is possible to get n colors from the palette but they loose their
+#' # association with specific altitudes
+#' etopo_colors(5)
+#'
+#' # The goal of these scales is to color maps and elevation models. Here is
+#' # one centered on Thailand, showcasing high mountains and deep trenches
+#' persp(thail, theta=30, phi=25, border=alpha("black", 0.2), expand=0.2,
+#'       col=etopo_map(persp_facets(thail$z)))
+#'
+#' \dontrun{library("rgl")
+#' # To get a perfect land/sea mapping, the colors need to be computed
+#' # exactly based on the input altitude values; set `exact.until` very high
+#' # do achieve this (but this makes the function slower of course).
+#' persp3d(thail, aspect=c(1,0.96,0.2), axes=FALSE, box=FALSE,
+#'         col=etopo_map(thail$z, exact.until=10^5))
+#' }
+#' @export
+etopo_scale <- function(exact.until=1000) {
+  bar <- interp_scale(colors=chroma::etopo$color, model="lab", interp="linear", values=chroma::etopo$altitude, exact.until=exact.until)
+}
+
+#' @param ... passed to \code{\link{etopo_scale}}.
+#' @rdname etopo_scale
+#' @export
+etopo_map <- function(x, ...) {
+  etopo_scale(...)(x)
+}
+
+#' @rdname etopo_scale
+#' @export
+etopo_palette <- function(exact.until=1000, ...) {
+  interp_palette(colors=chroma::etopo$color, model="lab", interp="linear", exact.until=exact.until, ...)
+}
+
+#' @rdname etopo_scale
+#' @export
+etopo_colors <- function(n, ...) {
+  etopo_palette(...)(n=n)
+}
+
+# TODO add ggplot versions
---
+++ data-raw/etopo.R
@@ -0,0 +1,50 @@
+#
+# Read ETOPO1 colors and bathy example
+#
+
+## Colors ----
+
+# Palette from http://soliton.vm.bytemark.co.uk/pub/cpt-city/ngdc/tn/ETOPO1.png.index.html
+
+# read postgis palette
+etopo_pg <- read.table("data-raw/ETOPO1.pg")
+
+# reverse the order, which is more logical, and parse colors
+etopo <- data.frame(
+  altitude=rev(etopo_pg[,1]),
+  color=rev(rgb(etopo_pg[,2:4], maxColorValue=255))
+)
+# set 0 to green
+etopo$altitude[24] <- 0
+
+# save the colors
+use_data(etopo, internal=FALSE, overwrite=TRUE)
+
+## Bathy ----
+
+library("ncdf4")
+nc <- nc_open("data-raw/etopo1.nc")
+lon <- ncvar_get(nc, "lon")
+lat <- ncvar_get(nc, "lat")
+alt <- ncvar_get(nc, "Band1")
+
+thai <- data.frame(lon=rep(lon, times=length(lat)), lat=rep(lat, each=length(lon)), alt=as.numeric(alt))
+
+library("dplyr")
+thai$lon <- plyr::round_any(thai$lon, 0.2)
+thai$lat <- plyr::round_any(thai$lat, 0.2)
+# thai$lon <- round(thai$lon, digits=1)
+# thai$lat <- round(thai$lat, digits=1)
+thai <- thai %>% group_by(lon, lat) %>% summarise(alt=mean(alt))
+
+# library("ggplot2")
+# ggplot(thai) + geom_raster(aes(lon, lat, fill=alt)) + coord_quickmap()
+
+thail <- list(
+  x=unique(thai$lon),
+  y=unique(thai$lat),
+  z=matrix(thai$alt, nrow=length(unique(thai$lon)), byrow=TRUE)
+)
+image(thail)
+
+use_data(thail, internal=FALSE, overwrite=TRUE)
