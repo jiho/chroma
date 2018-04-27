39ca025226e0e9eeebee38fe4c9b20e21c00158e
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-27 23:09:17 +0200

    Fix some tests

--- tests/testthat/test-na_handling.R
+++ tests/testthat/test-na_handling.R
@@ -2,30 +2,31 @@ context("NA handling")
 
 test_that("parsing functions handle NAs", {
   expect_equal(cmyk(NA), as.character(NA))
-  expect_equal(css(NA),  as.character(NA))
+  expect_equal(css(as.character(NA)),  as.character(NA))
   expect_equal(hcl(NA),  as.character(NA))
-  expect_equal(hex(NA),  as.character(NA))
+  expect_equal(hex(as.character(NA)),  as.character(NA))
   expect_equal(hsi(NA),  as.character(NA))
   expect_equal(hsl(NA),  as.character(NA))
   expect_equal(hsv(NA),  as.character(NA))
   expect_equal(lab(NA),  as.character(NA))
   expect_equal(rgb(NA),  as.character(NA))
+  expect_equal(rgba(NA),  as.character(NA))
   expect_equal(temperature(NA), as.character(NA))
 })
 
 test_that("conversion functions handle NAs", {
-  expect_equal(in_hex(NA), as.character(NA))
-  expect_equal(as.css(NA), as.character(NA))
-  expect_equal(as.temperature(NA), as.character(NA))
-  expect_equal(as.hex(NA), as.character(NA))
   expect_equivalent(as.cmyk(NA), matrix(ncol=4))
-  expect_equivalent(as.rgba(NA), matrix(ncol=4))
+  expect_equal(as.css(NA), as.character(NA))
   expect_equivalent(as.hcl(NA), matrix(ncol=3))
+  expect_equal(in_hex(NA), as.character(NA))
+  expect_equal(as.hex(NA), as.character(NA))
   expect_equivalent(as.hsi(NA), matrix(ncol=3))
   expect_equivalent(as.hsl(NA), matrix(ncol=3))
   expect_equivalent(as.hsv(NA), matrix(ncol=3))
   expect_equivalent(as.lab(NA), matrix(ncol=3))
   expect_equivalent(as.rgb(NA), matrix(ncol=3))
+  expect_equivalent(as.rgba(NA), matrix(ncol=4))
+  expect_equal(as.temperature(NA), as.numeric(NA))
 })
 
 
@@ -57,7 +58,7 @@ test_that("binary functions handle NAs", {
 
 test_that("scale functions handle NAs", {
   # in mapped values
-  expect_equal(color_scale()(NA), as.character(NA))
+  expect_equal(interp_scale()(NA), as.character(NA))
   expect_equal(brewer_scale()(NA), as.character(NA))
   expect_equal(viridis_scale()(NA), as.character(NA))
   expect_equal(cubehelix_scale()(NA), as.character(NA))
@@ -65,5 +66,5 @@ test_that("scale functions handle NAs", {
   expect_equal(chroma_scale()(NA), as.character(NA))
   expect_equal(light_scale()(NA), as.character(NA))
   # as input
-  expect_equal(color_scale(colors=c("white", "black"))(0), "#ffffff")
+  expect_equal(interp_scale(colors=c("white", NA, "black"))(0), "#ffffff")
 })
