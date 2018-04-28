a66afa392a0c284e007907b7115da4bc8efc10ae
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-28 17:01:28 +0200

    Change order to be alphabetical
    and make sure nothing is forgotten

--- tests/testthat/test-parse_reversibility.R
+++ tests/testthat/test-parse_reversibility.R
@@ -2,38 +2,6 @@ context("Parsing reversibility")
 
 x <- tolower(rainbow(10))
 
-test_that("rgb is reversible", {
-  expect_that(rgb(as.rgb(x), max=255), equals(as.hex(x)))
-})
-
-test_that("rgba is reversible", {
-  expect_that(rgba(as.rgba(x)), equals(x))
-})
-
-test_that("hsv is reversible", {
-  expect_that(hsv(as.hsv(x)), equals(as.hex(x)))
-})
-
-test_that("hsl is reversible", {
-  expect_that(hsl(as.hsl(x)), equals(as.hex(x)))
-})
-
-test_that("hsi is not reversible", {
-  expect_that(hsi(as.hsi(x)), not(equals(as.hex(x))))
-})
-
-test_that("hcl is reversible", {
-  expect_that(hcl(as.hcl(x)), equals(as.hex(x)))
-})
-
-test_that("lch is reversible", {
-  expect_that(lch(as.lch(x)), equals(as.hex(x)))
-})
-
-test_that("lab is reversible", {
-  expect_that(lab(as.lab(x)), equals(as.hex(x)))
-})
-
 test_that("cmyk is reversible", {
   expect_that(cmyk(as.cmyk(x)), equals(as.hex(x)))
 })
@@ -42,8 +10,38 @@ test_that("css is reversible", {
   expect_that(css(as.css(x)), equals(as.hex(x)))
 })
 
+test_that("hcl is reversible", {
+  expect_that(hcl(as.hcl(x)), equals(as.hex(x)))
+})
+test_that("lch is reversible", {
+  expect_that(lch(as.lch(x)), equals(as.hex(x)))
+})
+
 test_that("hex is reversible", {
   expect_that(hex(as.hex(x)), equals(as.hex(x)))
+})
+
+test_that("hsi is not reversible", {
+  expect_that(hsi(as.hsi(x)), not(equals(as.hex(x))))
+})
+
+test_that("hsl is reversible", {
+  expect_that(hsl(as.hsl(x)), equals(as.hex(x)))
+})
+
+test_that("hsv is reversible", {
+  expect_that(hsv(as.hsv(x)), equals(as.hex(x)))
+})
+
+test_that("lab is reversible", {
+  expect_that(lab(as.lab(x)), equals(as.hex(x)))
+})
+
+test_that("rgb is reversible", {
+  expect_that(rgb(as.rgb(x), max=255), equals(as.hex(x)))
+})
+test_that("rgba is reversible", {
+  expect_that(rgba(as.rgba(x)), equals(x))
 })
 
 test_that("temperature is not reversible", {
