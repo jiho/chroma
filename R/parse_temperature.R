376593c0d1756fc5e9dc50a7d94c85e839cef568
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-28 18:41:46 +0200

    Simplify the argument name of temperature()

--- R/parse_temperature.R
+++ R/parse_temperature.R
@@ -2,7 +2,7 @@
 #'
 #' Converts color temperature in Kelvin to a color vector.
 #'
-#' @param temperature light temperature in Kelvin, a numeric vector with values in \code{[1000, 40000]}. 1000 is candle light, 2000 is sunset light, 3200 is a tungsten light bulb, 4000 is a white fluorescent light tube, 5200 is average midday sunlight, 7000 is overcast daylight, 10000 is the color of a clear blue sky.
+#' @param x light temperature in Kelvin, a numeric vector with values in \code{[1000, 40000]}. 1000 is candle light, 2000 is sunset light, 3200 is a tungsten light bulb, 4000 is a white fluorescent light tube, 5200 is average midday sunlight, 7000 is overcast daylight, 10000 is the color of a clear blue sky.
 #'
 #' @template return_hex_colors
 #'
@@ -20,6 +20,6 @@
 #' plot(x=x, y=rep(0, 200), col=temperature(x), pch="|", cex=5, xaxt='n')
 #' axis(side=1, at=notable_temperatures, labels=TRUE, las=2)
 #' abline(v=notable_temperatures, lty="dotted")
-temperature <- function(temperature=5200) {
-  parse_color(temperature, "temperature")
+temperature <- function(x=5200) {
+  parse_color(x, "temperature")
 }
