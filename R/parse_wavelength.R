0db96a6a7795cdcd6a7e790fd4a2da9168f27dae
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-28 21:24:10 +0200

    Add parsing colors specified as wavelengths

---
+++ R/parse_wavelength.R
@@ -0,0 +1,16 @@
+#' Color Specification as Wavelength
+#'
+#' Parse colors specified as the wavelength of the light beam.
+#'
+#' @param x vector of wavelengths. The visible light, leading to actual colors, is between
+#'
+#' @template return_hex_colors
+#'
+#' @export
+#' @examples
+#'
+#'
+#'
+wavelength <- function(x) {
+
+}
