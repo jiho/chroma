70abbc69f78d6a6521f8d5c763b1deb175cd7b47
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-29 17:44:17 +0200

    Integrate hue shifting directly in the scale function and control it with an argument

--- R/scale_hue.R
+++ R/scale_hue.R
@@ -7,6 +7,7 @@
 #' @template param_chromacity
 #' @template param_lightness
 #' @inheritParams interp_scale
+#' @param full.circle when the range of hues specified in \code{h} spans 360°, this argument determines if the color scale should also span the full circle (which results in the same color being associated with different values, at the extremes of the scale), or not. By default it is set to FALSE to avoid this caveat. When \code{h} does not span 360°, this argument is ignored because the caveat disappears.
 #'
 #' @template details_hcl
 #'
@@ -51,7 +52,7 @@
 #'
 #' @importFrom scales rescale
 #' @export
-hue_scale <- function(h=c(0,360)+40, c=0.65, l=0.65, domain=c(0,1), reverse=FALSE) {
+hue_scale <- function(h=c(0,360)+40, c=0.65, l=0.65, domain=c(0,1), reverse=FALSE, full.circle=FALSE, na.value=NULL) {
   # check arguments
   if (length(h) != 2) {
     stop("h must be a vector of length 2, defining the range of hues to use.")
@@ -66,6 +67,7 @@ hue_scale <- function(h=c(0,360)+40, c=0.65, l=0.65, domain=c(0,1), reverse=FALS
     h2 <- h[1] + 360*sign(hue_range)
     warning(paste0("The range of hues chosen is wider than 360\u00B0 (",h[1]," -> ",h[2]," = ", hue_range,"\u00B0).\n  Several values would be mapped to the same hue, which is probably not desirable.\n  The range was clipped to ",h[1]," -> ",h2," = 360\u00B0."))
     h[2] <- h2
+    hue_range <- 360
   }
 
   # change the direction along the color wheel
@@ -75,6 +77,17 @@ hue_scale <- function(h=c(0,360)+40, c=0.65, l=0.65, domain=c(0,1), reverse=FALS
 
   # define the function
   f <- function(x) {
+    # force characters into factors to be able convert to numbers
+    if (is.character(x)) { x <- factor(x) }
+    # convert to number
+    x <- as.numeric(x)
+
+    # expand the domain to avoid cycling around the color grid
+    if (hue_range == 360 & !full.circle) {
+      n <- length(unique(x))
+      domain <- domain * c(1, (n+1)/n)
+    }
+
     # map colors
     colors <- hcl(h=scales::rescale(x, from=domain, to=h), c=c, l=l)
     }
@@ -88,9 +101,7 @@ hue_scale <- function(h=c(0,360)+40, c=0.65, l=0.65, domain=c(0,1), reverse=FALS
 #' @export
 hue_map <- function(x, ...) {
   # force characters into factors to be able to convert them to numeric
-  if (is.character(x)) {
-    x <- factor(x)
-  }
+  if (is.character(x)) { x <- factor(x) }
   # convert to numbers
   x <- as.numeric(x)
   # define the domain of the scale
