ac3ffd5da9578c87269d167e373596cfdc3e3502
Jean-Olivier Irisson <irisson@normalesup.org>
2018-04-30 09:51:18 +0200

    Make in_hex public

--- R/in_hex.R
+++ R/in_hex.R
@@ -1,18 +1,19 @@
-#' Force HEX representation
+#' Fast casting of colors into HEX strings
 #'
-#' Convert colors, including named ones as defined in the function \code{\link[grDevices]{colors}}, into their hex representation
+#' Convert colors, including named ones as defined in the function \code{\link[grDevices]{colors}}, into their hex representation. This function is mostly used internally by chroma.
 #'
 #' @template param_x_rcolors
 #'
 #' @template return_hex_colors
+#' @seealso \code{\link{hex}} for the same functionality offered in a more standard and more versatile way (albeit slower).
+#'
+#' @export
 #'
 #' @examples
 #' in_hex("pink")
 #' in_hex(c("pink", "#348A31", "darkblue"))
 #' in_hex(c("pink", NA))
 #' show_col(in_hex(colors()))
-#'
-#' @importFrom grDevices rgb
 in_hex <- function(x) {
   out <- grDevices::rgb(t(grDevices::col2rgb(x)), maxColorValue=255)
   # reinsert NAs (which are transformed into white by the previous command)
