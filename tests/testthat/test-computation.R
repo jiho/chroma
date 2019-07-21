737203d835ef35816aafa449168c4e14ea9caa30
Jean-Olivier Irisson <irisson@normalesup.org>
2019-07-21 21:07:55 +0200

    Reorder color computation tests to better match the functions

--- tests/testthat/test-computation.R
+++ tests/testthat/test-computation.R
@@ -9,29 +9,31 @@ models <- c("hcl", "lch", "hsi", "hsl", "hsv", "lab", "rgb", "lrgb")
 
 test_that("color mixing is replicable", {
   expect_equal(
-    mix(x[1], x[2], model=models),
-    c("#7F9837", "#7F9837", "#854FB9", "#924AD5", "#9558D0", "#C27D6F", "#AF6F70", "#C0797A")
-  )
-  expect_equal(
-    unlist(lapply(models, function(m) {average(x[1:2], model=m)})),
-    c("#00A4C5", "#00A4C5", "#4F9EA1", "#4AD1D5", "#58CCD0", "#C27D6F", "#AF6F70", "#FF4040")
-  )
-  expect_equal(
     unlist(lapply(models, function(m) {average(x, model=m)})),
     c("#009AB4", "#009AB4", "#46BD8F", "#41D39C", "#4BCE9D", "#BA668F", "#A85B8F", "#D23997")
   )
+
   expect_equal(
     blend(x[1], x[2], mode=c("multiply", "darken", "lighten", "screen", "overlay", "burn", "dodge")),
     c("#5F2828", "#5F4040", "#FF9EA0", "#FFB6B8", "#BE6E71", "#5F0000", "#FFD3D6")
   )
+
+  expect_equal(
+    mix(x[1], x[2], model=models),
+    c("#7F9837", "#7F9837", "#854FB9", "#924AD5", "#9558D0", "#C27D6F", "#AF6F70", "#C0797A")
+  )
 })
 
-# test_that("mixing and averaging are equivalent", {
-#   expect_equal(
-#     mix(x[1], x[2], model=models),
-#     unlist(lapply(models, function(m) {average(x[1:2], model=m)}))
-#   )
-# })
+test_that("mixing and averaging are equivalent in lab and RGB", {
+  expect_equal(
+    mix(x[1], x[2], model="lab"),
+    average(x[1:2], model="lab")
+  )
+  expect_equal(
+    mix(x[1], x[2], model="rgb"),
+    average(x[1:2], model="rgb")
+  )
+})
 
 test_that("distance measures are replicable", {
   expect_equal(
